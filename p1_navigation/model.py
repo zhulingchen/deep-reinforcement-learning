89dd455c8bb3afcb900d7689b996c0cf6f34f33e
Lingchen Zhu <zhulingchen@gmail.com>
2020-02-19 13:55:01 -0500

    finished Project: Navigation

---
+++ p1_navigation/model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fcn = nn.Sequential(
+            nn.Linear(state_size, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, action_size)  # linear activation
+        )
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        return self.fcn(state)
