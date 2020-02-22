f521d140b316156c419fd9d84674a640d75f9185
Lingchen Zhu <zhulingchen@gmail.com>
2020-02-22 15:25:01 -0500

    implemented double deep Q-learning

--- p1_navigation/dqn_agent.py
+++ p1_navigation/dqn_agent.py
@@ -12,15 +12,16 @@ BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR = 5e-4               # learning rate 
-UPDATE_EVERY = 4        # how often to update the network
+LR = 5e-4               # learning rate
+TRAIN_EVERY = 4         # how often to train a batch
+UPDATE_EVERY = 16       # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Agent():
     """Interacts with and learns from the environment."""
 
-    def __init__(self, state_size, action_size, seed):
+    def __init__(self, state_size, action_size, seed, use_double=False):
         """Initialize an Agent object.
         
         Params
@@ -28,10 +29,12 @@ class Agent():
             state_size (int): dimension of each state
             action_size (int): dimension of each action
             seed (int): random seed
+            use_double (bool): whether to use double deep Q-learning
         """
         self.state_size = state_size
         self.action_size = action_size
         self.seed = random.seed(seed)
+        self.use_double = use_double
 
         # Q-Network
         self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
@@ -46,14 +49,18 @@ class Agent():
     def step(self, state, action, reward, next_state, done):
         # Save experience in replay memory
         self.memory.add(state, action, reward, next_state, done)
-        
-        # Learn every UPDATE_EVERY time steps.
-        self.t_step = (self.t_step + 1) % UPDATE_EVERY
-        if self.t_step == 0:
-            # If enough samples are available in memory, get random subset and learn
-            if len(self.memory) > BATCH_SIZE:
-                experiences = self.memory.sample()
-                self.learn(experiences, GAMMA)
+
+        # Update time step
+        self.t_step += 1
+
+        # If enough samples are available in memory, get random subset and learn
+        if (len(self.memory) >= BATCH_SIZE) and (self.t_step % TRAIN_EVERY == 0):
+            experiences = self.memory.sample()
+            self.learn(experiences, GAMMA)
+
+        # Update target network every UPDATE_EVERY time steps.
+        if self.t_step % UPDATE_EVERY == 0:
+            self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)
 
     def act(self, state, eps=0.):
         """Returns actions for given state as per current policy.
@@ -86,7 +93,11 @@ class Agent():
         states, actions, rewards, next_states, dones = experiences
 
         # Get max predicted Q values (for next states) from target model
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        if self.use_double:
+            _, idx = self.qnetwork_local(next_states).detach().max(1)
+            Q_targets_next = self.qnetwork_target(next_states).detach().gather(1, idx.unsqueeze(1))
+        else:
+            Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
         
         # Compute Q targets for current states 
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
@@ -101,9 +112,6 @@ class Agent():
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
-
-        # ------------------- update target network ------------------- #
-        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)                     
 
     def soft_update(self, local_model, target_model, tau):
         """Soft update model parameters.
